14cda1383f53af50ae91a55b5f1826806e0a4626
Narges Jangholi <jangholi83@gmail.com>
2026-01-20 16:00:49 +0330

    refactor: Improve contact query readability with Pythonic formatting

--- src/crud/contacts.py
+++ src/crud/contacts.py
@@ -35,7 +35,14 @@ def get_all(db: Session) -> list[Contact]:
     :param db: SQLAlchemy session object.
     :return: List of Contact objects ordered by creation date (descending).
     """
-    return db.query(Contact).order_by(func.lower(Contact.first_name)).all()
+    return (
+        db.query(Contact)
+        .order_by(
+        func.lower(Contact.first_name),
+        func.lower(Contact.last_name),
+        )
+        .all()
+    )
 
 
 def get_by_id(db: Session, contact_id: int) -> Contact | None:
